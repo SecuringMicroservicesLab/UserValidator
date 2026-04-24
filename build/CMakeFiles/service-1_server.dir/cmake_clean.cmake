9d13e34df9a5c6abd36e5a34d31dcdf73716327c
owenschu7 <owenschu7@gmail.com>
2026-04-24 11:18:02 -0700

    init commit

---
+++ build/CMakeFiles/service-1_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/service-1_server.dir/link.d"
+  "CMakeFiles/service-1_server.dir/src/main.cpp.o"
+  "CMakeFiles/service-1_server.dir/src/main.cpp.o.d"
+  "service-1_server"
+  "service-1_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/service-1_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/service-1_server.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for service-1_server.
+# This may be replaced when dependencies are built.
